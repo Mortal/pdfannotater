0c3979a90c2ef87d303aab60ec470737ac809707
Mathias Rav <rav@cs.au.dk>
2015-02-01 13:50:53 +0100

    Move main stuff out

--- pdfannotator/__init__.py
+++ pdfannotator/__init__.py
@@ -717,22 +717,3 @@ class MainWindow(QtGui.QMainWindow):
         self.boldAction.setChecked(font.weight() == QtGui.QFont.Bold)
         self.italicAction.setChecked(font.italic())
         self.underlineAction.setChecked(font.underline())
-
-
-def main():
-    global a
-    app = QtGui.QApplication(sys.argv)
-
-    a = MainWindow()
-    a.show()
-    if len(app.arguments()) > 1:
-        pep = os.path.splitext(str(app.arguments()[1]))[0]+".pep"
-        if os.path.exists(pep):
-            a.doLoad(pep)
-        else:
-            a.doNewProject(app.arguments()[1])
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
---
+++ pdfannotator/__main__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+from PyQt4 import QtGui, QtCore, uic
+from pdfannotator import MainWindow
+
+
+def main():
+    global a
+    app = QtGui.QApplication(sys.argv)
+
+    a = MainWindow()
+    a.show()
+    if len(app.arguments()) > 1:
+        pep = os.path.splitext(str(app.arguments()[1]))[0] + ".pep"
+        if os.path.exists(pep):
+            a.doLoad(pep)
+        else:
+            a.doNewProject(app.arguments()[1])
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
